fabcc8509e088d1bc35fc66a46fce1deee27cc6e
Anikthedev-Official <yoiwannajinksbegam@gmail.com>
2025-12-28 15:08:50 +0600

    more files

--- server.js
+++ server.js
@@ -27,8 +27,44 @@ const MIME = {
   ".ttf": "font/ttf", // fonts!
   ".oft": "font/oft", // MORE FONTS!
   ".ttfc": "font/collection", // EVEN MORE FONTS!
+  ".jpeg": "image/jpeg", // ew jpeg 1
+  ".bmp": "image/bmp", // i have free time ok?
+  ".tiff": "image/tiff", // just learned about this one
+  ".tif": "image/tiff", // just learned about this one too
+  ".ico": "image/x-icon", // favicons and stuff i guess
+  ".cur": "image/x-icon", // this WAS NOT nessesary but here we are
+  ".avif": "image/avif", // where the FUCK do these come from
+  ".heif": "image/heif", // another image format that i dont care about 
+  ".heic": "image/heic", // yet another image format that i dont care about
+  ".jp2": "image/jp2", // OOOOLD i think
+  ".jxl": "image/jxl", // JPEG NEEDS TO STOP
+  ".pam": "image/x-portable-anymap", // nu.
+  ".pbm": "image/x-portable-bitmap", // twins
+  ".pgm": "image/x-portable-graymap", // more twins
+  ".ppm": "image/x-portable-pixmap", // even more twins
+  ".pnm": "image/x-portable-anymap", // last twins
+  ".pcx": "image/x-pcx", // PCX image format but last twins i swear
+  ".tga": "image/x-tga", // *cough*
+  ".iff": "image/iff", // asdg fhkdjhgsdjkghljhjlkfsliysgui ainuagufhjnhafj hgfhn ff ,anj,fnjgkhl dfshl hgsf,njf,k sagjlk harsghufsjdfhnb sd,k jdfgksdfgsjhksdf sfhgsf sfh,jhfjsfgj 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+  ".ras": "image/x-cmu-raster", // CMU Raster image format, what even is this
+  ".sgi": "image/x-sgi", // SGI image format, what even is this too
+  ".rgb": "image/x-rgb", // RGB image format, the lights? rgb lights?
+  "xbm": "image/x-xbitmap", // X BitMap, what even is this either
+  ".xpm": "image/x-xpixmap", // X PixMap, are you serious?
+  ".wbmp": "image/vnd.wap.wbmp", // Wireless Bitmap, seriously????
+  ".sun": "image/x-sun-raster", // THE SUN
+  ".pct": "image/pict", // die die die die die
+  ".pict": "image/pict", // die die die die die die
+  ".img": "image/img", // image image format ofc
+  ".apng": "image/apng", // pngs but cooler
+  ".mng": "video/x-mng", // beans
+  ".fli": "video/x-fli", // adding a p makes it flip
+  ".flc": "video/x-flc", // *insert anything here*
+  ".ani": "application/x-navi-animation", // animated cursors i think
+  ".svgz": "image/svg+xml", // svgs but cooler i think
+  ".pdf": "application/pdf", // documents and i think im done now
   ".png": "image/png", // co- no.
-  ".jpg": "image/jpeg", // ew jpeg
+  ".jpg": "image/jpeg", // ew jpeg 2
   ".svg": "image/svg+xml", // vector images, YEAH BABY!
   ".woff": "font/woff", // web fonts? what are those?
   ".woff2": "font/woff2", // again?, what are those?
